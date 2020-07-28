a6ccf9c3446a2adfc79dc324c69a2d4628428665
kuznet.i.sova <kis7425910@gmail.com>
2020-07-28 14:41:45 +0300

    fix start errors

---
+++ StateAndDataFlow/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  StateAndDataFlow
+//
+//  Created by Alexey Efimov on 27.07.2020.
+//  Copyright © 2020 Alexey Efimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject private var timer = TimeCounter()
+    @EnvironmentObject var user: UserManager
+    
+    var body: some View {
+        VStack {
+            Text("Hi, \(user.name)")
+                .font(.largeTitle)
+                .offset(x: 0, y: 100)
+            Text("\(timer.counter)")
+                .font(.largeTitle)
+                .offset(x: 0, y: 200)
+            Spacer()
+            
+            VStack {
+                ButtonView(timer: timer)
+            }
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+        .environmentObject(UserManager())
+    }
+}
+
+struct ButtonView: View {
+    @ObservedObject var timer: TimeCounter
+    
+    var body: some View {
+        VStack {
+            Button(action: { self.timer.startTemer() }) {
+                Text("\(timer.buttonTitle)")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+            }
+            .frame(width: 200, height: 60)
+            .background(Color.red)
+            .cornerRadius(20)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20, style: .continuous)
+                    .stroke(Color.black, lineWidth: 4)
+            )
+        }
+    }
+}
